dcdad438162c6a71b8894d177ffc6b0260c4e5b3
Tim Hartmann <tfhartmann@gmail.com>
2016-08-31 12:58:43 -0400

    Moved the ECS stuff to it's own module

--- main.tf
+++ main.tf
@@ -5,57 +5,12 @@
 #$ export AWS_SECRET_ACCESS_KEY="asecretkey"
 #$ export AWS_DEFAULT_REGION="us-west-2"
 
-resource "aws_instance" "server" {
-    ami                  = "${lookup(var.ami, "${var.region}")}"
-    instance_type        = "${var.instance_type}"
-    key_name             = "${var.key_name}"
-    count                = "${var.servers}"
-    vpc_security_group_ids = ["${aws_security_group.ecs.id}"]
-    subnet_id            = "${var.subnet_id}"
-    #iam_instance_profile = "AmazonECSContainerInstanceRole"
-    iam_instance_profile = "${aws_iam_instance_profile.ecs_test_profile.name}"
-    depends_on           = ["aws_iam_instance_profile.ecs_test_profile", "aws_security_group.ecs"]
-    user_data = <<EOF
-#!/bin/bash
-echo ECS_CLUSTER=${aws_ecs_cluster.lunchbot.name} >> /etc/ecs/ecs.config
-EOF
-    connection {
-        user = "${lookup(var.user, var.platform)}"
-        key_file = "${var.key_path}"
-    }
-
-    #Instance tags
-    tags {
-        Name = "${var.tagName}-${count.index}"
-    }
-}
-
-resource "aws_security_group" "ecs" {
-  name        = "ecs-sg"
-  description = "Container Instance Allowed Ports"
-  vpc_id      = "${var.vpc_id}"
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "ecs-sg"
-  }
-}
-
-# Make this a var that an get passed in?
-resource "aws_ecs_cluster" "lunchbot" {
-  name = "infra-services"
+module "ecs-cluster" {
+  source       = "./terraform-ecs-cluster"
+  cluster_name = "infra-services"
+  servers      = 1
+  subnet_id    = "subnet-6e101446"
+  vpc_id       = "vpc-99e73dfc"
 }
 
 resource "aws_ecs_task_definition" "ecs-lunchbot" {
@@ -71,9 +26,9 @@ resource "template_file" "lunchbot-container" {
 }
 
 resource "aws_ecs_service" "slack_lunchbot" {
-  name = "slack_lunchbot"
-  cluster = "${aws_ecs_cluster.lunchbot.id}"
+  name            = "slack_lunchbot"
+  cluster         = "${module.ecs-cluster.cluster_id}"
   task_definition = "${aws_ecs_task_definition.ecs-lunchbot.arn}"
-  desired_count = 1
-  depends_on = ["aws_iam_instance_profile.ecs_test_profile"]
+  desired_count   = 1
+  #depends_on = ["aws_iam_instance_profile.ecs_test_profile"]
 }
--- variables.tf
+++ variables.tf
@@ -1,58 +1,5 @@
-variable "ami" {
-  description           = "ECS AWS AMI Id, if you change, make sure it is compatible with instance type, not all AMIs allow all instance types "
-    default = {
-        us-east-1      = "ami-f53ea5e2"
-        us-west-2      = "ami-181cd678"
-        eu-west-1      = "ami-21f9ba41"
-        eu-central-1   = "ami-3eb94851"
-        ap-northeast-1 = "ami-5bfd2438"
-        ap-southeast-1 = "ami-9689bdf5"
-        ap-southeast-2 = "ami-3f10d25e"
-    }
-}
-
-variable "key_name" {
-  description = "SSH key name in your AWS account for AWS instances."
-    default   = "amazonhosts"
-}
-
-variable "key_path" {
-  description  = "Path to the private key specified by key_name."
-    default    = {
-      key_path = "/Users/alaric/amazonhosts.pem"
-    }
-}
-
-variable "region" {
-  default     = "us-east-1"
-  description = "The region of AWS, for AMI lookups."
-}
-
-variable "servers" {
-  default     = "1"
-  description = "The number of servers to launch."
-}
-
-variable "instance_type" {
-  default     = "t2.micro"
-  description = "AWS Instance type, if you change, make sure it is compatible with AMI, not all AMIs allow all instance types "
-}
-
-variable "tagName" {
-  default     = "ECS Node"
-  description = "Name tag for the servers"
-}
 
 variable "slack_url" {
   default     = "https://hooks.slack.com/services/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"
   description = "URL to pass as an environment variable to the lunchbox bot"
-}
-
-variable "subnet_id" {
-  default     = "subnet-6e101446"
-  description = "The AWS Subnet ID in which you want to delpoy your instances"
-}
-variable "vpc_id" {
-  default     = "vpc-99e73dfc"
-  description = "The AWS VPC ID which you want to deploy your instances"
 }
