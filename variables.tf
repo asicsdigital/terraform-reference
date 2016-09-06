3b33de1f73c4998e1de18865564887d81e4e52ed
Tim Hartmann <tfhartmann@gmail.com>
2016-09-06 16:52:35 -0400

    Removed default slack_url

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 
 variable "slack_url" {
-  default     = "https://hooks.slack.com/services/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"
   description = "URL to pass as an environment variable to the lunchbox bot"
 }
